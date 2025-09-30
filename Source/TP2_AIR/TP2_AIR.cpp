533d8e2f6614b84446055079c6c3877c2ded8fa9
ROBIN ABELA <rabela@etu.uqac.ca>
2025-09-30 15:43:46 -0400

    Initial commit

---
+++ Source/TP2_AIR/TP2_AIR.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TP2_AIR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP2_AIR, "TP2_AIR" );
